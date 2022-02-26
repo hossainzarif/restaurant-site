0771d3714bbde56849eb02abebf9d1f16a860ba9
hossainzarif <zarifh493@gmail.com>
2022-02-26 11:29:18 +0600

    navbar added

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from 'react'
 
-import './Navbar.css';
-
+import { GiHamburgerMenu } from 'react-icons/gi'
+import { MdOutlineRestaurantMenu } from 'react-icons/md'
+import './Navbar.css'
+import images from '../../constants/images'
 const Navbar = () => (
-  <div>
-    Navbar
-  </div>
-);
+  <nav className='app__navbar'>
+    <div className='app__navbar-logo'>
+      <img src={images.gericht} alt='app logo' />
+    </div>
+    <ul className='app__navbar-links'>
+      <li className='p__opensans'>
+        <a href='#home'>Home</a>
+      </li>
+      <li className='p__opensans'>
+        <a href='#home'>About</a>
+      </li>
 
-export default Navbar;
+      <li className='p__opensans'>
+        <a href='#home'>Menu</a>
+      </li>
+
+      <li className='p__opensans'>
+        <a href='#home'>Awards</a>
+      </li>
+      <li className='p__opensans'>
+        <a href='#home'>Reviews</a>
+      </li>
+      <li className='p__opensans'>
+        <a href='#home'>Contacts</a>
+      </li>
+    </ul>
+  </nav>
+)
+
+export default Navbar
