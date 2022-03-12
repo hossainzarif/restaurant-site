c083be7f5cfbfa3989a9ee25640ac1309d5e1abb
hossainzarif <zarifh493@gmail.com>
2022-03-13 01:41:09 +0600

    intro added

--- frontend/src/container/Intro/Intro.jsx
+++ frontend/src/container/Intro/Intro.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React, { useRef, useState } from 'react'
+import { BsF } from 'react-icons'
+import { meal } from '../../constants'
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs'
+import './Intro.css'
 
-import './Intro.css';
+const Intro = () => {
+  const vidRef = useRef(null)
+  const [play, setPlay] = useState(false)
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+  return (
+    <div className='app__video'>
+      <video src={meal} type='video/mp4' loop controls={false} muted />
 
-export default Intro;
+      <div className='app__-overlay flex__center'>
+        <div className='app__video-overlay_circle flex__center'>
+          <BsFillPlayFill />
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Intro
