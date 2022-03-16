14ee01616470d1d746bd642f1356b43b62d92c28
hossainzarif <zarifh493@gmail.com>
2022-03-16 21:49:18 +0600

    scroll function added

--- frontend/src/container/Gallery/Gallery.jsx
+++ frontend/src/container/Gallery/Gallery.jsx
@@ -3,9 +3,24 @@ import React from 'react'
 import { SubHeading } from '../../components'
 import { images } from '../../constants'
 import './Gallery.css'
-import { BsInstagram, BsArrowLeftShort, BsArrowRight } from 'react-icons/bs'
+import {
+  BsInstagram,
+  BsArrowLeftShort,
+  BsArrowRight,
+  BsArrowRightShort,
+} from 'react-icons/bs'
 
 const Gallery = () => {
+  const scrollRef = React.useRef(null)
+  const scroll = (direct) => {
+    const { current } = scrollRef
+    if (direct === 'left') {
+      current.scrollLeft -= 300
+    } else {
+      current.scrollRight += 300
+    }
+  }
+
   return (
     <div className='app__gallery flex__center'>
       <div className='app__gallery-content'>
@@ -17,6 +32,17 @@ const Gallery = () => {
         <button type='button' className='custom__button'>
           Show More
         </button>
+      </div>
+
+      <div className='app__gallery-images'>
+        <div className='app__gallery-images_container' ref={scrollRef}></div>
+      </div>
+      <div className='app__gallery-images_arrows'>
+        <BsArrowLeftShort
+          className='gallery__arrow-icon'
+          onClick={() => scroll('left')}
+        />
+        <BsArrowRightShort className='gallery__arrow-icon' />
       </div>
     </div>
   )
