0732a44710459c1cae906365a83d7be7a6c4dd95
hossainzarif <zarifh493@gmail.com>
2022-03-16 13:59:52 +0600

    gallery heading added

--- frontend/src/container/Gallery/Gallery.jsx
+++ frontend/src/container/Gallery/Gallery.jsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React from 'react'
 
-import './Gallery.css';
+import { SubHeading } from '../../components'
+import { images } from '../../constants'
+import './Gallery.css'
+import { BsInstagram, BsArrowLeftShort, BsArrowRight } from 'react-icons/bs'
 
-const Gallery = () => (
-  <div>
-    Gallery
-  </div>
-);
+const Gallery = () => {
+  return (
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <SubHeading title='Instagram' />
+        <h1 className='headtext__cormorant'> Photo Gallery</h1>
+        <p className='p__opensans' style={{ color: '#AAA' }}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit auctor sit .
+        </p>
+        <button type='button' className='custom__button'>
+          Show More
+        </button>
+      </div>
+    </div>
+  )
+}
 
-export default Gallery;
+export default Gallery
